2eb102917b6e282890ecf400e450f7780917c9d3
RealAntEngineer <lafourmiedu35@gmail.com>
2024-03-05 18:19:24 +0100

    solving rounding issue

--- src/main/java/com/rae/creatingspace/utilities/CSUtil.java
+++ src/main/java/com/rae/creatingspace/utilities/CSUtil.java
@@ -1,8 +1,6 @@
 package com.rae.creatingspace.utilities;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.RoundingMode;
 
 public class CSUtil {
 
@@ -18,41 +16,31 @@ public class CSUtil {
     public static String scientificNbrFormatting(Float toRound, int digit) {
         return scientificNbrFormatting(BigDecimal.valueOf(toRound),digit);
     }
-
-    public static String scientificNbrFormatting(Integer toRound, int digit) {
-        return scientificNbrFormatting((float) toRound, digit);
-    }
-    //using BigDecimal is a better way
-
-    //it's still not prefect
     public static String scientificNbrFormatting(BigDecimal toRound, int digit) {
         if (toRound.floatValue() == 0) return "0";
-        digit = Math.max(0, Math.abs(digit - 1));
-        int nbrOf10pow = (int) Math.log10(toRound.floatValue());
-        if (toRound.floatValue() < 1) {
-            nbrOf10pow--;
+        BigDecimal rounded = toRound;
+        int nbrOf10Pow = 0;
+        while (rounded.floatValue() > 1000 || rounded.floatValue() < 0.1) {
+            if (rounded.floatValue() >1) {
+                rounded = rounded.movePointLeft(3);
+                nbrOf10Pow +=3;
+            }
+            else if (rounded.floatValue() < 1){
+                rounded = rounded.movePointRight(3);
+                nbrOf10Pow -=3;
+            }
         }
-        BigDecimal powOf10Value = new BigDecimal(BigInteger.ONE,-nbrOf10pow);//+1 to obtain a nbr bwn 0.1 and 0.9 ?
         String unitCoef;
-        int roundedPow;
-        if (nbrOf10pow >= 6) {
+        if (nbrOf10Pow >= 6) {
             unitCoef = "M";
-            roundedPow = nbrOf10pow - 6;
-        } else if (nbrOf10pow >= 3) {
+        } else if (nbrOf10Pow >= 3) {
             unitCoef = "k";
-            roundedPow = nbrOf10pow - 3;
-        } else if (nbrOf10pow >= 0) {
+        } else if (nbrOf10Pow >= 0) {
             unitCoef = "";
-            roundedPow = nbrOf10pow;
         } else {
             unitCoef = "m";
-            roundedPow = 3 + nbrOf10pow;
         }
-        //rounding
-        BigDecimal to1Value = toRound.divide(powOf10Value,RoundingMode.HALF_UP);
-        BigDecimal roundedValue = to1Value.setScale(digit,RoundingMode.HALF_UP);
-        BigDecimal powOf10Remaining = new BigDecimal(BigInteger.ONE,-roundedPow);
-        BigDecimal finalValue = roundedValue.multiply(powOf10Remaining);
-        return finalValue.floatValue()+unitCoef;
+        String floatString = String.valueOf(rounded.floatValue());
+        return floatString.subSequence(0,rounded.floatValue()<Math.pow(10,digit-1)?digit+1:digit)+unitCoef;
     }
 }
