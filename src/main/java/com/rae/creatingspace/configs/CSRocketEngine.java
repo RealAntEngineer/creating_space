8b640613dbd6bd45db2e996f77d2da376e889393
RealAntEngineer <lafourmiedu35@gmail.com>
2024-03-05 01:09:41 +0100

    prep work for hotfix

--- src/main/java/com/rae/creatingspace/configs/CSRocketEngine.java
+++ src/main/java/com/rae/creatingspace/configs/CSRocketEngine.java
@@ -4,7 +4,7 @@ public class CSRocketEngine extends CSConfigBase{
     public final ConfigInt bigRocketEngineThrust = i((int) (500000*9.81),0,"bigRocketEngineThrust", Comments.bigRocketEngineThrust);
     public final ConfigInt smallRocketEngineThrust = i((int) (10000*9.81),0,"smallRocketEngineThrust", Comments.smallRocketEngineThrust);
     public final ConfigInt methaloxISP = i(365,0,"methaloxISP", Comments.methaloxISP);
-    public final ConfigFloat ISPModifier = f(1F,0.1F,"ISPModifier", Comments.ISPModifier);
+    public final ConfigFloat ISPModifier = f(1.5F, 0.1F, "ISPModifier", Comments.ISPModifier);
 
     @Override
     public String getName() {
