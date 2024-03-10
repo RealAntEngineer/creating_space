dd5e58c4dcb5668418646f3bc14689fe18a9c016
RealAntEngineer <lafourmiedu35@gmail.com>
2024-03-10 18:36:31 +0100

    removing DimensionEffect registration on the server side

--- src/main/java/com/rae/creatingspace/CreatingSpaceClient.java
+++ src/main/java/com/rae/creatingspace/CreatingSpaceClient.java
@@ -1,6 +1,7 @@
 package com.rae.creatingspace;
 
 import com.rae.creatingspace.init.PonderInit;
+import com.rae.creatingspace.init.graphics.DimensionEffectInit;
 import com.rae.creatingspace.init.graphics.PartialModelInit;
 import com.rae.creatingspace.init.graphics.ParticleTypeInit;
 import net.minecraftforge.eventbus.api.IEventBus;
