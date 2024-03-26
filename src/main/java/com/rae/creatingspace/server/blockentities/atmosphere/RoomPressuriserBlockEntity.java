080722b48e313104c1e251e1ddafced48420be79
RealAntEngineer <lafourmiedu35@gmail.com>
2024-03-26 13:15:28 +0100

    adding a test block (the room pressuriser) and a way to find the shape of a room

---
+++ src/main/java/com/rae/creatingspace/server/blockentities/atmosphere/RoomPressuriserBlockEntity.java
@@ -0,0 +1,44 @@
+package com.rae.creatingspace.server.blockentities.atmosphere;
+
+import com.rae.creatingspace.init.ingameobject.EntityInit;
+import com.rae.creatingspace.server.blocks.atmosphere.RoomPressuriserBlock;
+import com.rae.creatingspace.server.entities.RoomAtmosphere;
+import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+
+public class RoomPressuriserBlockEntity extends KineticBlockEntity {
+    public RoomPressuriserBlockEntity(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
+        super(typeIn, pos, state);
+    }
+
+    RoomAtmosphere room;
+
+    public void tryRoom() {
+        if (room != null && room.isAlive()) {
+            room.regenerateRoom(getBlockPos().relative(getBlockState().getValue(RoomPressuriserBlock.FACING)));
+        } else {
+            room = new RoomAtmosphere(EntityInit.ATMOSPHERE_ENTITY.get(), getLevel());
+            room.setPos(Vec3.atCenterOf(this.getBlockPos()));
+            if (level != null) {
+                level.addFreshEntity(room);
+            }
+        }
+    }
+
+    @Override
+    protected void read(CompoundTag compound, boolean clientPacket) {
+        super.read(compound, clientPacket);
+        int id = compound.getInt("room");
+        room = id == Integer.MIN_VALUE ? null : (RoomAtmosphere) level.getEntity(id);
+    }
+
+    @Override
+    protected void write(CompoundTag compound, boolean clientPacket) {
+        compound.putInt("room", room == null ? Integer.MIN_VALUE : room.getId());
+        super.write(compound, clientPacket);
+    }
+}
--- src/main/java/com/rae/creatingspace/server/entities/RoomAtmosphere.java
+++ src/main/java/com/rae/creatingspace/server/entities/RoomAtmosphere.java
@@ -1,5 +1,6 @@
 package com.rae.creatingspace.server.entities;
 
+import com.rae.creatingspace.server.blockentities.atmosphere.RoomPressuriserBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
@@ -10,6 +11,8 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.network.NetworkHooks;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -18,45 +21,107 @@ import java.util.Queue;
 
 public class RoomAtmosphere extends Entity {
 
-    Frontier frontier;
+    RoomShape shape;
+    private int o2amount;
 
     public RoomAtmosphere(EntityType<?> entityType, Level level) {
         super(entityType, level);
         noPhysics = true;
+        shape = new RoomShape(new ArrayList<>());
     }
 
     //the tick method will be called by the sealer be
-    static int searchPerTick = 200;
+    static int searchPerTick = 100;
+    static int maxSearch = 10;
+    int nbrOfSearch = 0;
+    // schedule a search for maximum 10 ticks and 200 block search every tick
+
     //TODO first test with only 1 sealer then try with several the system to merge and separate rooms
 
     //TODO compatibility with contraptions
     //TODO make a search for every sealer inside -> list of sealer
 
+    boolean shouldContinueSearch = false;
+    boolean hasSearchThisTick = false;
+    //TODO making special behavior for blocks and be inside an oxygen room.
+    // plants will filter (absorbing C02 and releasing 02, living will consume 02 and release C02)
     ArrayList<BlockPos> roomSealers = new ArrayList<>();
+    Queue<BlockPos> toVist = new ArrayDeque<>();
+    ArrayList<BlockPos> visited = new ArrayList<>();
 
-    public void regenerateRoom(BlockPos originalPos) {
-        Queue<BlockPos> toVist = new ArrayDeque<>();
-        ArrayList<BlockPos> visited = new ArrayList<>();
-        toVist.add(originalPos);
+    //TODO make a regenerateRoom and a searchFrontier methode (regenerateRoom will only initiate the call, searchFrontier will be private)
+    public void regenerateRoom(BlockPos firstPos) {
 
-        //if it ends without length condition then it's a closed room
-        while (!toVist.isEmpty() && visited.size() < 200) {
-            System.out.println("coucou");
-            BlockPos pos = toVist.poll();
-            frontier.add(pos);
-            visited.add(pos);
-            for (Direction dir : Direction.values()) {
-                if (!visited.contains(pos.relative(dir)) && canGoThrough(level.getBlockState(pos.relative(dir)), dir)) {
-                    toVist.add(pos.relative(dir));
-                }
-            }
-        }
-        AABB potentialBound = frontier.getEncapsulingBox();
-        if (potentialBound != null) {
-            setBoundingBox(potentialBound);
-        }
+        shape = new RoomShape(searchTopology(firstPos));
     }
 
+    private List<AABB> searchTopology(BlockPos start) {
+        Queue<BlockPos> toVist = new ArrayDeque<>();
+        toVist.add(start);
+        ArrayList<AABB> tempRoom = new ArrayList<>();
+        while (!toVist.isEmpty() && size(tempRoom) < 1000) {
+            BlockPos tempPos = toVist.poll();
+            boolean contain = false;
+            for (AABB aabb : tempRoom) {
+                if (aabb.contains(Vec3.atCenterOf(tempPos))) {
+                    contain = true;
+                }
+            }
+            if (!contain) {
+                AABB tempAabb = new AABB(tempPos);
+
+                boolean canContinue = true;
+                while (canContinue && tempAabb.getSize() < 1000) {
+                    canContinue = false;
+                    for (Direction dir : Direction.values()) {
+                        //make a condition here to avoid looping when no walls
+                        //verify that it's in the range of a room pressurizer ? or just size
+                        //TODO change the place of the while
+                        //verify for positive dir and negative ones
+                        AABB expansion = tempAabb.expandTowards(dir.getNormal().getX(), dir.getNormal().getY(), dir.getNormal().getZ());
+
+                        List<BlockPos> collectedPos = new ArrayList<>();
+                        BlockPos.betweenClosedStream(expansion.contract(1, 1, 1)).forEach(
+                                (pos) -> {
+                                    collectedPos.add(pos.immutable());
+                                }
+                        );
+                        boolean canBeExpandedToward = true;
+                        for (BlockPos pos : collectedPos) {
+                            if (!tempAabb.contains(Vec3.atCenterOf(pos)) && !canGoThrough(level.getBlockState(pos), dir)) {
+                                canBeExpandedToward = false;
+                            }
+                        }
+                        //if we can expand toward that way we add it to the current box
+                        // else we add every air block of this slice to vist
+                        if (canBeExpandedToward) {
+                            tempAabb = expansion;
+                            canContinue = true;
+                        } else {
+                            for (BlockPos pos : collectedPos) {
+                                if (!tempAabb.contains(Vec3.atCenterOf(pos)) && canGoThrough(level.getBlockState(pos), dir)) {
+                                    toVist.add(pos);
+                                } else if (!tempAabb.contains(Vec3.atCenterOf(pos))) {
+                                    if (level.getBlockEntity(pos) instanceof RoomPressuriserBlockEntity rp) {
+                                        roomSealers.add(pos);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                tempRoom.add(tempAabb);
+            }
+        }
+        System.out.println(tempRoom);
+        return tempRoom;
+    }
+
+    private int size(List<AABB> aabbs) {
+        AABB aabb = new RoomShape(aabbs).getEncapsulatingBox();
+        if (aabb == null) return 0;
+        return (int) (aabb.getXsize() * aabb.getYsize() * aabb.getZsize());
+    }
     private boolean canGoThrough(BlockState blockState, Direction dir) {
         return blockState.isAir();
     }
@@ -78,45 +143,64 @@ public class RoomAtmosphere extends Entity {
 
     @Override
     public Packet<?> getAddEntityPacket() {
-        return null;
+        return NetworkHooks.getEntitySpawningPacket(this);
     }
 
     public void addBlockToFrontier(BlockPos pos) {
-        frontier.add(pos);
+        shape.add(pos);
     }
 
     public static EntityType.Builder<?> build(EntityType.Builder<?> builder) {
         @SuppressWarnings("unchecked")
         EntityType.Builder<RoomAtmosphere> entityBuilder =
                 (EntityType.Builder<RoomAtmosphere>) builder;
-        return entityBuilder.sized(1, 1);
+        return entityBuilder;
     }
-
     @Override
     public void tick() {
         super.tick();
-        List<Entity> entitiesInside = frontier.getEntitiesInside(this, level);
-        for (Entity entity :
-                entitiesInside) {
-            if (entity instanceof LivingEntity living) {
-                //cancel Oxygen suffocation event if present ??
 
+        if (!level.isClientSide()) {
+            if (hasFrontier()) {
+                List<Entity> entitiesInside = shape.getEntitiesInside(this, level);
+                for (Entity entity :
+                        entitiesInside) {
+                    if (entity instanceof LivingEntity living) {
+                        consumeO2();
+                        //cancel Oxygen suffocation event if present ??
+
+                    }
+                }
+                /*if (shouldContinueSearch) {
+                    regenerateRoom();
+                }*/
+            } else {
+                //regenerateRoom();
             }
+            hasSearchThisTick = false;
         }
     }
 
     public boolean hasFrontier() {
-        return !frontier.listOfBox.isEmpty();
+        return !shape.listOfBox.isEmpty();
     }
 
-    private class Frontier {
+    public void consumeO2() {
+
+    }
+
+    public boolean isRoomBreathable() {
+        return false;
+    }
+
+    private class RoomShape {
         ArrayList<AABB> listOfBox;//should be a list of points that define a frontier
 
         float xRot;
         float yRot;
         float zRot;
 
-        Frontier(List<AABB> listOfBox) {
+        RoomShape(List<AABB> listOfBox) {
             this.listOfBox = new ArrayList<>(listOfBox);
         }
 
@@ -126,7 +210,7 @@ public class RoomAtmosphere extends Entity {
             add(firstBlock);
         }
 
-        public AABB getEncapsulingBox() {
+        public AABB getEncapsulatingBox() {
             Double minX = null;
             Double minY = null;
             Double minZ = null;
