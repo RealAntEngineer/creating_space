0bf22c78c1b107c8955a6fd8b714696285546333
RealAntEngineer <lafourmiedu35@gmail.com>
2024-06-05 21:43:49 +0200

    adding a way to get registry access on server side (part of the datapack driven registry handling)
    Signed-off-by: RealAntEngineer <lafourmiedu35@gmail.com>

--- src/main/java/com/rae/creatingspace/server/event/DataEventHandler.java
+++ src/main/java/com/rae/creatingspace/server/event/DataEventHandler.java
@@ -1,15 +1,24 @@
 package com.rae.creatingspace.server.event;
 
 import com.rae.creatingspace.CreatingSpace;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Objects;
+
 @Mod.EventBusSubscriber(modid = CreatingSpace.MODID)
 public class DataEventHandler {
+    //if it's null, then your server is distant and your on the client
+    public static RegistryAccess.Frozen registryAccess = null;
     @SubscribeEvent
     public static void onLoadWorld(LevelEvent.Load event) {
         LevelAccessor world = event.getLevel();
@@ -22,19 +31,24 @@ public class DataEventHandler {
         CreatingSpace.DESIGN_SAVED_DATA.playerLogin(player);
     }
 
-    /*@SubscribeEvent
-    public static void onCreatingRegistry(NewRegistryEvent event){
-        event.create(new RegistryBuilder<PowerPackType>().setName(MiscInit.Keys.POWER_PACK_TYPE.location()).disableSaving()
-                .allowModification().dataPackRegistry(PowerPackType.DIRECT_CODEC, PowerPackType.DIRECT_CODEC));
-    }*/
-    /*@SubscribeEvent
-    public static void onSeverStarted(ServerStartedEvent event){
-        MinecraftServer server = event.getServer();
-        final RegistryAccess registries = server.registryAccess();
-        registries.registryOrThrow(MiscInit.Keys.POWER_PACK_TYPE).entrySet().forEach(
-                resourceKeyPowerPackTypeEntry -> {
-                    System.out.println(resourceKeyPowerPackTypeEntry.getKey());
-                }
-        );
-    }*/
+
+    @SubscribeEvent
+    public static void onServerStarting(ServerStartingEvent event) {
+        registryAccess = event.getServer().registryAccess();
+    }
+
+    /**
+     * @param registryKey the registryKey for a common registry
+     * @param <T>         the registry object type
+     * @return the registry
+     */
+    public static <T> Registry<T> getSideAwareRegistry(ResourceKey<Registry<T>> registryKey) {
+        if (registryAccess != null) {
+            return registryAccess.ownedRegistry(registryKey).orElseThrow();
+        } else {
+            return Objects.requireNonNull(Minecraft.getInstance().getConnection())
+                    .registryAccess().registry(registryKey)
+                    .orElseThrow();
+        }
+    }
 }
