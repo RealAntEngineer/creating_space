1f7d9e44a0f8478f5b93cb1bd368fd19ec958af3
RealAntEngineer <lafourmiedu35@gmail.com>
2024-08-27 23:23:05 +0200

    temp fix for recipes

--- src/main/java/com/rae/creatingspace/server/blockentities/RocketEngineBlockEntity.java
+++ src/main/java/com/rae/creatingspace/server/blockentities/RocketEngineBlockEntity.java
@@ -5,6 +5,8 @@ import com.rae.creatingspace.api.IMass;
 import com.rae.creatingspace.api.design.PropellantType;
 import com.rae.creatingspace.configs.CSConfigs;
 import com.rae.creatingspace.init.ingameobject.PropellantTypeInit;
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
@@ -14,7 +16,9 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
-public abstract class RocketEngineBlockEntity extends BlockEntity {
+import java.util.List;
+
+public abstract class RocketEngineBlockEntity extends SmartBlockEntity {
 
     public int getIsp() {
         return (int) (getPropellantType().getMaxISP() * getEfficiency());
@@ -35,12 +39,17 @@ public abstract class RocketEngineBlockEntity extends BlockEntity {
         int thrust = 1000;
         PropellantType propellantType = PropellantTypeInit.METHALOX.get();
         Float efficiency = 1f;
-        Integer size = 100;
         int mass = 0;
 
         public NbtDependent(BlockEntityType<?> type, BlockPos pos, BlockState state) {
             super(type, pos, state);
         }
+
+        @Override
+        public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
+
+        }
+
         @Override
         public float getEfficiency() {
             return efficiency;
@@ -61,9 +70,9 @@ public abstract class RocketEngineBlockEntity extends BlockEntity {
         }
 
         @Override
-        protected void saveAdditional(CompoundTag nbt) {
+        protected void write(CompoundTag nbt, boolean clientPacket) {
             nbt.putInt("thrust", thrust);
-            nbt.putInt("size", size);
+            nbt.putInt("mass", mass);
             nbt.putFloat("efficiency", efficiency);
             try {
                 nbt.put("propellantType", ResourceLocation.CODEC.encodeStart(NbtOps.INSTANCE,
@@ -71,11 +80,11 @@ public abstract class RocketEngineBlockEntity extends BlockEntity {
             } catch (Exception ignored){
                 nbt.put("propellantType", ResourceLocation.CODEC.encodeStart(NbtOps.INSTANCE,PropellantTypeInit.METHALOX.getId() ).get().orThrow());
             }
-            super.saveAdditional(nbt);
+            super.write(nbt, clientPacket);
         }
 
         @Override
-        public void load(CompoundTag nbt) {
+        public void read(CompoundTag nbt, boolean clientPacket) {
             super.load(nbt);
             setFromNbt(nbt);
         }
@@ -99,8 +108,9 @@ public abstract class RocketEngineBlockEntity extends BlockEntity {
         }
 
         @Override
-        public void saveToItem(ItemStack itemStack) {
-            super.saveToItem(itemStack);
+        public void initialize() {
+            notifyUpdate();
+            super.initialize();
         }
     }
 
@@ -123,6 +133,11 @@ public abstract class RocketEngineBlockEntity extends BlockEntity {
         public BigEngine(BlockEntityType<?> type, BlockPos pos, BlockState state) {
             super(type, pos, state);
         }
+
+        @Override
+        public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
+
+        }
     }
 
     public static class SmallEngine extends RocketEngineBlockEntity{
@@ -144,6 +159,11 @@ public abstract class RocketEngineBlockEntity extends BlockEntity {
 
         public SmallEngine(BlockEntityType<?> type, BlockPos pos, BlockState state) {
             super(type, pos, state);
+        }
+
+        @Override
+        public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
+
         }
 
     }
