c877d963326ef16be5c571f0eee1708a59c8f9ee
Real Ant Engineer <lafourmiedu35@gmail.com>
2025-06-04 12:03:27 +0200

    code cleaning

--- src/main/java/com/rae/creatingspace/CreatingSpace.java
+++ src/main/java/com/rae/creatingspace/CreatingSpace.java
@@ -12,7 +12,6 @@ import com.rae.creatingspace.init.graphics.MenuTypesInit;
 import com.rae.creatingspace.init.graphics.ParticleTypeInit;
 import com.rae.creatingspace.init.ingameobject.*;
 import com.rae.creatingspace.init.worldgen.CarverInit;
-import com.rae.creatingspace.legacy.saved.UnlockedDesignManager;
 import com.rae.creatingspace.init.CSContraptionType;
 import com.rae.creatingspace.content.event.IgniteOnPlace;
 import com.rae.creatingspace.legacy.utilities.data.MassOfBlockReader;
@@ -41,7 +40,7 @@ public class CreatingSpace {
     public static final String MODID = "creatingspace" ;
 
     public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MODID);
-    public static final UnlockedDesignManager DESIGN_SAVED_DATA = new UnlockedDesignManager();
+
     static {
         REGISTRATE.setTooltipModifierFactory(item -> {
             return new ItemDescription.Modifier(item, FontHelper.Palette.STANDARD_CREATE).
---
+++ src/main/java/package-info.java
@@ -0,0 +1,8 @@
+@NonnullDefault
+package com.rae.creatingspace;
+
+import org.lwjgl.system.NonnullDefault;
+
+//TODO reduce visibility where it's possible.
+//TODO remove unused non api methods on all release branches (unused methods are often used for development purposes)
+//TODO make some more explicit javadoc
