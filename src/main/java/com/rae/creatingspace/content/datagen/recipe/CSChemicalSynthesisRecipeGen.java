9e7067094b2a3fb498943e83d08b8429c9c26301
kalwantspizza <kalwantspizza@gmail.com>
2025-07-17 20:03:26 -0400

    Datagen for Chemical Synthesis & Electrolysis

---
+++ src/main/java/com/rae/creatingspace/content/datagen/recipe/CSChemicalSynthesisRecipeGen.java
@@ -0,0 +1,49 @@
+package com.rae.creatingspace.content.datagen.recipe;
+
+import com.rae.creatingspace.CreatingSpace;
+import com.rae.creatingspace.content.recipes.chemical_synthesis.ChemicalSynthesisRecipe;
+import com.rae.creatingspace.init.ingameobject.FluidInit;
+import com.rae.creatingspace.init.RecipeInit;
+import com.simibubi.create.api.data.recipe.ProcessingRecipeGen;
+import com.simibubi.create.content.processing.recipe.HeatCondition;
+import com.simibubi.create.content.processing.recipe.ProcessingRecipeParams;
+import com.simibubi.create.foundation.recipe.IRecipeTypeInfo;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.material.Fluids;
+
+import java.util.concurrent.CompletableFuture;
+
+@SuppressWarnings("unused")
+public class CSChemicalSynthesisRecipeGen extends ProcessingRecipeGen<
+        ProcessingRecipeParams,
+        ChemicalSynthesisRecipe,
+        ChemicalSynthesisRecipe.Builder<ChemicalSynthesisRecipe>
+        > {
+
+    public CSChemicalSynthesisRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> lookup) {
+        super(output, lookup, CreatingSpace.MODID);
+    }
+
+    @Override
+    protected IRecipeTypeInfo getRecipeType() {
+        return RecipeInit.CHEMICAL_SYNTHESIS;
+    }
+
+    @Override
+    protected ChemicalSynthesisRecipe.Builder<ChemicalSynthesisRecipe> getBuilder(ResourceLocation id) {
+        return new ChemicalSynthesisRecipe.Builder<>(ChemicalSynthesisRecipe::new, id);
+    }
+
+    {
+        GeneratedRecipe METHANE = create("methane_synthesis", b -> b
+                .require(FluidInit.LIQUID_CO2.get(), 100)
+                .require(FluidInit.LIQUID_HYDROGEN.get(), 286)
+                .output(FluidInit.LIQUID_METHANE.get(), 80)
+                .output(Fluids.WATER, 90)
+                .requiresHeat(HeatCondition.HEATED)
+                .duration(200)
+        );
+    }
+}
