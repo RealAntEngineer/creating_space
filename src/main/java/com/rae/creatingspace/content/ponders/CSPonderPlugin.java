9dbe407f4f19d565c7ef13e9f7fe9771dfb1420c
Real Ant Engineer <lafourmiedu35@gmail.com>
2026-03-01 20:45:30 +0100

    fixing the engine recipe (only the generated files not the generator)

--- src/main/java/com/rae/creatingspace/content/ponders/CSPonderPlugin.java
+++ src/main/java/com/rae/creatingspace/content/ponders/CSPonderPlugin.java
@@ -28,25 +28,25 @@ public class CSPonderPlugin implements PonderPlugin {
 
     @Override
     public void registerTags(PonderTagRegistrationHelper<ResourceLocation> helper) {
-        AllCreatePonderTags.register(helper);
+        //AllCreatePonderTags.register(helper);
     }
 
     @Override
     public void registerSharedText(SharedTextRegistrationHelper helper) {
-        helper.registerSharedText("rpm8", "8 RPM");
+        /*helper.registerSharedText("rpm8", "8 RPM");
         helper.registerSharedText("rpm16", "16 RPM");
         helper.registerSharedText("rpm16_source", "Source: 16 RPM");
         helper.registerSharedText("rpm32", "32 RPM");
 
         helper.registerSharedText("movement_anchors", "With the help of Super Glue, larger structures can be moved.");
         helper.registerSharedText("behaviour_modify_value_panel", "This behaviour can be modified using the value panel");
-        helper.registerSharedText("storage_on_contraption", "Inventories attached to the Contraption will pick up their drops automatically");
+        helper.registerSharedText("storage_on_contraption", "Inventories attached to the Contraption will pick up their drops automatically");*/
     }
 
     @Override
     public void indexExclusions(IndexExclusionHelper helper) {
-        helper.excludeBlockVariants(ValveHandleBlock.class, AllBlocks.COPPER_VALVE_HANDLE.get());
-        helper.excludeBlockVariants(PostboxBlock.class, AllBlocks.PACKAGE_POSTBOXES.get(DyeColor.WHITE).get());
-        helper.excludeBlockVariants(TableClothBlock.class, AllBlocks.TABLE_CLOTHS.get(DyeColor.WHITE).get());
+        //helper.excludeBlockVariants(ValveHandleBlock.class, AllBlocks.COPPER_VALVE_HANDLE.get());
+        //helper.excludeBlockVariants(PostboxBlock.class, AllBlocks.PACKAGE_POSTBOXES.get(DyeColor.WHITE).get());
+        //helper.excludeBlockVariants(TableClothBlock.class, AllBlocks.TABLE_CLOTHS.get(DyeColor.WHITE).get());
     }
 }
