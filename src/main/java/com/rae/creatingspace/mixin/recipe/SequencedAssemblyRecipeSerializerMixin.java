71d3504cfeeda2d86d43055ddb80cc3a2fb1291f
Real Ant Engineer <lafourmiedu35@gmail.com>
2026-04-09 22:13:33 +0200

    create 6.0.9

--- src/main/java/com/rae/creatingspace/mixin/recipe/SequencedAssemblyRecipeSerializerMixin.java
+++ src/main/java/com/rae/creatingspace/mixin/recipe/SequencedAssemblyRecipeSerializerMixin.java
@@ -9,18 +9,14 @@ import io.netty.buffer.ByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 @Mixin(value = SequencedAssemblyRecipeSerializer.class)
 public abstract class SequencedAssemblyRecipeSerializerMixin {
@@ -29,20 +25,31 @@ public abstract class SequencedAssemblyRecipeSerializerMixin {
     private static final StreamCodec<ByteBuf, List<String>> STRING_LIST_STREAM_CODEC =
             ByteBufCodecs.fromCodec(Codec.list(Codec.STRING));
 
-    //to fix it inject into the stream codec and do the same as addToCodec
-    /*@Inject(method = "fromNetwork", at = @At("RETURN"), cancellable = true)
-    public void readKeepNbt(RegistryFriendlyByteBuf buffer, CallbackInfoReturnable<SequencedAssemblyRecipe> cir) {
-        SequencedAssemblyRecipe recipe = cir.getReturnValue();
-        ((IMoreNbtConditions) recipe).setKeepNbt(new ArrayList<>(STRING_LIST_STREAM_CODEC.decode(buffer)));
-        ((IMoreNbtConditions) recipe).setMachNbt(new ArrayList<>(STRING_LIST_STREAM_CODEC.decode(buffer)));
-        cir.setReturnValue(recipe);
-    }
+    @Inject(method = "streamCodec", at = @At("RETURN"), cancellable = true)
+    public void addToStreamCodec(CallbackInfoReturnable<StreamCodec<RegistryFriendlyByteBuf, SequencedAssemblyRecipe>> cir) {
+        StreamCodec<RegistryFriendlyByteBuf, SequencedAssemblyRecipe> sc = StreamCodec.composite(
+                cir.getReturnValue(), r -> r,
+                STRING_LIST_STREAM_CODEC, r -> {
+                    if (r instanceof IMoreNbtConditions moreNbtConditions) {
+                        return moreNbtConditions.getKeepNbt();
+                    }
+                    return  List.of();
+                },
+                STRING_LIST_STREAM_CODEC, r -> {
+                    if (r instanceof IMoreNbtConditions moreNbtConditions) {
+                        return moreNbtConditions.getMachNbt();
+                    }
+                    return  List.of();
+                },
+                 (recipe, mach, keep) -> {
+                    ((IMoreNbtConditions) recipe).setKeepNbt(new ArrayList<>(keep));
+                    ((IMoreNbtConditions) recipe).setMachNbt(new ArrayList<>(mach));
+                    return recipe;
+                }
+        );
+        cir.setReturnValue(sc);
 
-    @Inject(method = "toNetwork", at = @At("RETURN"))
-    public void writeKeepNbt(RegistryFriendlyByteBuf buffer, SequencedAssemblyRecipe recipe, CallbackInfo ci) {
-        STRING_LIST_STREAM_CODEC.encode(buffer, ((IMoreNbtConditions) recipe).getKeepNbt());
-        STRING_LIST_STREAM_CODEC.encode(buffer, ((IMoreNbtConditions) recipe).getMachNbt());
-    }*/
+    }
 
     @Inject(method = "codec", at = @At("RETURN"), cancellable = true)
     public void addToCodec(CallbackInfoReturnable<MapCodec<SequencedAssemblyRecipe>> cir) {
