c6b2b13f3f9678137f69c7ccfa282d0946f7433f
Real Ant Engineer <lafourmiedu35@gmail.com>
2026-05-08 00:49:56 +0200

    the caves... what did you do mojang
    Signed-off-by: Real Ant Engineer <lafourmiedu35@gmail.com>

--- src/main/java/com/rae/creatingspace/content/worldgen/noise/HermiteCubicInterpolator.java
+++
@@ -1,6 +0,0 @@
-package com.rae.creatingspace.content.worldgen.noise;
-
-import net.minecraft.world.level.levelgen.DensityFunction;
-import net.minecraft.world.level.levelgen.DensityFunctions;
-import net.minecraft.world.level.levelgen.NoiseChunk;
-
